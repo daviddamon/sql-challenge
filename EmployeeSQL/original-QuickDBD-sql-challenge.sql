e24df961956e5a127c2219beb2c2eaabe3a8dc8d
Dave Damon <dave@daviddamon.com>
2020-08-19 17:16:52 -0400

    schema debugged

--- EmployeeSQL/original-QuickDBD-sql-challenge.sql
+++ EmployeeSQL/original-QuickDBD-sql-challenge.sql
@@ -5,8 +5,8 @@
 -- schema for sql-challenge homework
 
 CREATE TABLE "dept_emp" (
-    "emp_no" string   NOT NULL,
     "dept_no" string   NOT NULL,
+    "emp_no" string   NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
         "dept_no"
      )
@@ -14,20 +14,20 @@ CREATE TABLE "dept_emp" (
 
 CREATE TABLE "dept_manager" (
     "emp_no" string   NOT NULL,
-    "dept_no" string   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_no"
-     )
+    "dept_no" string   NOT NULL
 );
 
 CREATE TABLE "departments" (
     "dept_no" string   NOT NULL,
-    "dept_name" string   NOT NULL
+    "dept_name" string   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
 );
 
 CREATE TABLE "employees" (
     "emp_no" string   NOT NULL,
-    "emp_title_id" string   NOT NULL,
+    "title_id" string   NOT NULL,
     "birth_date" date   NOT NULL,
     "first_name" string   NOT NULL,
     "last_name" string   NOT NULL,
@@ -45,11 +45,11 @@ CREATE TABLE "salaries" (
 
 CREATE TABLE "titles" (
     "title_id" string   NOT NULL,
-    "title" string   NOT NULL
+    "title" string   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
 );
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
@@ -60,9 +60,12 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "dept_manager" ("dept_no");
 
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "dept_emp" ("emp_no");
+
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
-REFERENCES "employees" ("emp_title_id");
+REFERENCES "employees" ("title_id");
 
--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -2,48 +2,99 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/i8wR2Z
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
--- Note: Originally imported from QuickDBD, but heavily edited.
 -- schema for sql-challenge homework
 
-CREATE TABLE titles (
-    title_id VARCHAR NOT NULL,
-    title VARCHAR NOT NULL,
-	PRIMARY KEY (title_id)
-);
-
-CREATE TABLE employees (
-    emp_no VARCHAR NOT NULL,
-    emp_title_id VARCHAR NOT NULL,
-    birth_date DATE NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    sex VARCHAR NOT NULL,
-    hire_date DATE NOT NULL,
-    PRIMARY KEY (emp_no),
-	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
-);
-
-CREATE TABLE salaries (
-    emp_no VARCHAR NOT NULL,
-    salary INT NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
-
+DROP TABLE IF EXISTS departments CASCADE;
 CREATE TABLE departments (
-    dept_no VARCHAR NOT NULL,
-    dept_name VARCHAR NOT NULL,
-	PRIMARY KEY (dept_no)
+    dept_no VARCHAR(30) NOT NULL,
+    dept_name VARCHAR(30) NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (dept_no)
 );
 
+COPY departments (dept_no, dept_name)
+FROM 'C:\Users\dave\Data_Bootcamp\Homework\sql-challenge\EmployeeSQL\data\departments.csv'
+DELIMITER ',' CSV HEADER;
+
+
+DROP TABLE IF EXISTS titles CASCADE;
+CREATE TABLE titles (
+    title_id VARCHAR(30) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (title_id)
+);
+
+COPY titles (title_id, title)
+FROM 'C:\Users\dave\Data_Bootcamp\Homework\sql-challenge\EmployeeSQL\data\titles.csv'
+DELIMITER ',' CSV HEADER;
+
+
+DROP TABLE IF EXISTS employees CASCADE;
+CREATE TABLE employees (
+    emp_no INT NOT NULL,
+    title_id VARCHAR(30) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    sex VARCHAR(30) NOT NULL,
+    hire_date DATE NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (emp_no)
+);
+
+COPY employees (emp_no, title_id, birth_date, first_name, last_name, sex, hire_date)
+FROM 'C:\Users\dave\Data_Bootcamp\Homework\sql-challenge\EmployeeSQL\data\employees.csv'
+DELIMITER ',' CSV HEADER;
+
+
+DROP TABLE IF EXISTS salaries CASCADE;
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL
+);
+
+COPY salaries (emp_no, salary)
+FROM 'C:\Users\dave\Data_Bootcamp\Homework\sql-challenge\EmployeeSQL\data\salaries.csv'
+DELIMITER ',' CSV HEADER;
+
+
+DROP TABLE IF EXISTS dept_emp CASCADE;
 CREATE TABLE dept_emp (
-    emp_no VARCHAR NOT NULL,
-    dept_no VARCHAR NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES dept_emp(dept_no)
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(30) NOT NULL
 );
 
+COPY dept_emp (emp_no, dept_no)
+FROM 'C:\Users\dave\Data_Bootcamp\Homework\sql-challenge\EmployeeSQL\data\dept_emp.csv'
+DELIMITER ',' CSV HEADER;
+
+
+DROP TABLE IF EXISTS dept_manager CASCADE;
 CREATE TABLE dept_manager (
-    emp_no VARCHAR NOT NULL,
-    dept_no VARCHAR NOT NULL,
-  	FOREIGN KEY (dept_no) REFERENCES dept_emp(dept_no)
+    dept_no VARCHAR(30) NOT NULL
+	emp_no INT NOT NULL,
 );
+
+COPY dept_manager (dept_no, emp_no)
+FROM 'C:\Users\dave\Data_Bootcamp\Homework\sql-challenge\EmployeeSQL\data\dept_manager.csv'
+DELIMITER ',' CSV HEADER;
+
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE employees ADD CONSTRAINT fk_employees_title_id FOREIGN KEY(title_id)
+REFERENCES titles (title_id);
+
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+
 
